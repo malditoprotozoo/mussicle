b603c73bee6934653c262894845aaae9ed6d3e56
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-02-19 22:57:51 -0300

    creado nuevo js y añadida la configuración de firebase

---
+++ assets/js/functions.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyBNw_KqGZwp8xg-J1r-e8spIIXkglnlhF0",
+  authDomain: "mussicle-app.firebaseapp.com",
+  databaseURL: "https://mussicle-app.firebaseio.com",
+  projectId: "mussicle-app",
+  storageBucket: "mussicle-app.appspot.com",
+  messagingSenderId: "1077193889162"
+};
